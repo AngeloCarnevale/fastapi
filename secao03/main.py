1e046341c5530a2222808ee0921c1ee910545e0a
kaliIinux <angelo.carnevale@hotmail.com>
2023-07-20 22:21:59 -0300

    Métodos básicos da API

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,12 @@
+from typing import List, Optional
+
+from fastapi.responses import JSONResponse
+from fastapi import Response
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+
+from models import Curso
 
 
 app = FastAPI()
@@ -17,23 +25,49 @@ cursos = {
 }
 
 
-@app.get('/')
-async def get_cursos():
-    return "Home"
-
 @app.get('/cursos')
 async def get_cursos():
     return cursos
 
 @app.get('/cursos/{curso_id}')
 async def get_cursos(curso_id: int):
-    curso = cursos[curso_id]
+    try:
+        curso = cursos[curso_id]
+        return curso
     
+    # Trata id inexistente
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.') 
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
     return curso
 
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id:int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}')
+    
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id:int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content={})
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com o id {curso_id}')
+    
 
 if __name__ == "__main__":
     import uvicorn
     
-    
+
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    
