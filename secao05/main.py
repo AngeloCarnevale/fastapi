cfc3e95af50b3c05a977a637692ba664319fee1b
kaliIinux <angelo.carnevale@hotmail.com>
2023-08-01 20:58:08 -0300

    Adding CORS

--- secao05/main.py
+++ secao05/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
 from core.configs import settings
 from api.v1.api import api_router
 
@@ -7,6 +8,18 @@ from api.v1.api import api_router
 app: FastAPI = FastAPI(title='Users API - FastAPI ASQL Model')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
