f7b22f037deb0e45373d94904043715aa5565c30
AngeloCarnevale <>
2023-11-08 09:58:05 -0300

    finishing all cruds/routes

---
+++ autenticação-e-autorização/api/v1/api.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from api.v1.endpoints import artigo
+from api.v1.endpoints import user
+
+
+api_router = APIRouter()
+
+api_router.include_router(artigo.router, prefix='/artigos', tags=['Artigos'])
+api_router.include_router(user.router, prefix='/usuarios', tags=['Usuários'])
