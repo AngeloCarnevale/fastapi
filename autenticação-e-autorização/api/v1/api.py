e5c546ab662b73ec19e8232f2959234f8c01b813
AngeloCarnevale <angelo.carnevale@hotmail.com>
2023-11-13 16:58:44 -0300

    up

--- autenticação-e-autorização/api/v1/api.py
+++ autenticação-e-autorização/api/v1/api.py
@@ -2,11 +2,9 @@ from fastapi import APIRouter
 
 from api.v1.endpoints import artigo
 from api.v1.endpoints import user
-from api.v1.endpoints import github_provider
 
 
 api_router = APIRouter()
 
 api_router.include_router(artigo.router, prefix='/artigos', tags=['Artigos'])
 api_router.include_router(user.router, prefix='/usuarios', tags=['Usuários'])
-api_router.include_router(github_provider.router, prefix='/github', tags=['Github Provider'])
--- autenticação-e-autorização/core/configs.py
+++ autenticação-e-autorização/core/configs.py
@@ -1,17 +1,21 @@
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.decl_api import DeclarativeMeta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'mysql+asyncmy://root:1234@localhost:3307/fastapi'
+    DB_URL: str = os.environ.get('DB_URL') 
     DB_BASE_MODEL: DeclarativeMeta = declarative_base()
     
     JWT_SECRET: str = 'tFQKIFuyPmSjCFax1gph83uioRs3BGeUsut8nFkMfJc'
     
     ALGORITHM: str = 'HS256'
-    # 60 minutos * 24 horas * 7 dias => 1 semana
+    
     ACESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     
     class Config:
